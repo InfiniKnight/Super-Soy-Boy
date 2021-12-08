23f4b0af876fb5206a42328b6caa628ff88734ac
Devon Payette <djstu01@hotmail.ca>
2021-12-08 17:19:59 -0500

    hazards

--- New Unity Project/Assets/Scripts/SoyBoyController.cs
+++ New Unity Project/Assets/Scripts/SoyBoyController.cs
@@ -124,6 +124,7 @@ public class SoyBoyController : MonoBehaviour
         // 1
         input.x = Input.GetAxis("Horizontal");
         input.y = Input.GetAxis("Jump");
+        animator.SetFloat("Speed", Mathf.Abs(input.x));
         // 2
         if (input.x > 0f)
         {
@@ -136,10 +137,12 @@ public class SoyBoyController : MonoBehaviour
         if (input.y >= 1f)
         {
             jumpDuration += Time.deltaTime;
+            animator.SetBool("IsJumping", true);
         }
         else
         {
             isJumping = false;
+            animator.SetBool("IsJumping", false);
             jumpDuration = 0f;
         }
         if (PlayerIsOnGround() && isJumping == false)
@@ -147,6 +150,7 @@ public class SoyBoyController : MonoBehaviour
             if (input.y > 0f)
             {
                 isJumping = true;
+                animator.SetBool("IsOnWall", false);
             }
         }
         if (jumpDuration > jumpDurationThreshold) input.y = 0f;
@@ -190,8 +194,19 @@ public class SoyBoyController : MonoBehaviour
         rb.velocity = new Vector2(xVelocity, yVelocity);
         if (IsWallToLeftOrRight() && !PlayerIsOnGround() && input.y == 1)
         {
-            rb.velocity = new Vector2(-GetWallDirection()
-            * speed * 0.75f, rb.velocity.y);
+            rb.velocity = new Vector2(-GetWallDirection() * speed
+            * 0.75f, rb.velocity.y);
+            animator.SetBool("IsOnWall", false);
+            animator.SetBool("IsJumping", true);
+        }
+        else if (!IsWallToLeftOrRight())
+        {
+            animator.SetBool("IsOnWall", false);
+            animator.SetBool("IsJumping", true);
+        }
+        if (IsWallToLeftOrRight() && !PlayerIsOnGround())
+        {
+            animator.SetBool("IsOnWall", true);
         }
         if (isJumping && jumpDuration < jumpDurationThreshold)
         {
